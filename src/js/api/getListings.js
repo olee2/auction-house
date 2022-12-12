3c4f9fd7e7f6fca9ed025c7657c595a99dd0d17f
olee2 <oleaeik@protonmail.com>
2022-12-12 09:20:57 +0100

    Initial sort

--- src/js/api/getListings.js
+++ src/js/api/getListings.js
@@ -3,7 +3,7 @@ import { apiCall } from './index.js';
 export const getListings = async (order) => {
   order === '' ? (order = 'desc') : null;
 
-  const url = `https://nf-api.onrender.com/api/v1/auction/listings?_bids=true&_active=true`;
+  const url = `https://nf-api.onrender.com/api/v1/auction/listings?_bids=true&_active=true&sort=created&sortOrder=asc`;
 
   const data = await apiCall(url);
 
